182cdc76bb8192749aad4a4c7e8b162c9617336d
unknown <asadfarzand71@gmail.com>
2026-06-02 21:04:51 +0500

    eks version corrected

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -40,7 +40,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version."
   type        = string
-  default     = "1.31"
+  default     = "1.30"
 }
 
 variable "node_instance_type" {
