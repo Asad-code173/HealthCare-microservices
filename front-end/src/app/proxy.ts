21435ee018da11b56a9c4e71dde57f38eaa04e11
unknown <asadfarzand71@gmail.com>
2026-06-14 04:59:51 +0500

    proxy.ts chnages

--- front-end/src/app/proxy.ts
+++ front-end/src/app/proxy.ts
@@ -87,7 +87,7 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/((?!_next|api/health|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)((?!/health).*)",
   ],
 };
